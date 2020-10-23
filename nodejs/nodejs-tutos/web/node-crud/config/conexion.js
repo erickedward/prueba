b0cb5197111076039dece26dfb720feb61818a6b
erick <erickedward@gmail.com>
2020-10-22 21:48:46 -0400

    Eliminar carpeta node

--- nodejs/nodejs-tutos/web/node-crud/config/conexion.js
+++
@@ -1,5 +0,0 @@
-let mongoose = require('mongooses');
-
-// mongoose.connect('mongodb://username:password@port.mlab.com:15446/databasename', { useMongoClient: true }); //mongodb://localhost:27017/crud
-mongoose.connect('mongodb://localhost:27017/crud', { useMongoClient: true }); //mongodb://localhost:27017/crud
-module.exports = mongoose;
